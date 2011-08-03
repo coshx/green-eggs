10d911a98934aa77ea067067884ef9100a02380f
Gabe Kopley <gabe@coshx.com>
2011-08-03 15:55:24 -0600

    bootstrapped rails 3.1 / mongoid / heroku app

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc5'
+
+gem 'haml'
+gem 'heroku'
+gem 'json'
+gem 'mongoid'
+gem 'SystemTimer'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "~> 3.1.0.rc"
+  gem 'coffee-rails', "~> 3.1.0.rc"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreenEggs::Application.config.secret_token = 'f68d7912faceaa4c3384cc61062f5c326b144d851b0f656cdf52f6a377c116d16567d654cabfd679383cced2b84b17132c830c26aecc00a191ad0079b901d133'
