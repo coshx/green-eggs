ba682a5f32089f0e0c3e110180c5c495749770ba
Gabe Kopley <gabe@coshx.com>
2011-08-04 10:51:08 -0600

    aah

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,9 @@ GreenEggs::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {
+    :host => 'greeneg.gs'
+  }
 
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
