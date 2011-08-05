519a54d3e9219ec05c1cae7497fe02e560522c09
Gabe Kopley <gabe@coshx.com>
2011-08-05 11:31:40 -0600

    show preferential results, with synonyms

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -1,8 +1,10 @@
 module PollsHelper
 
-  def calculate_results(poll)
+  def calculate_results(poll, related=false)
     results = {}
+ 
     poll.ballots.each do |b|
+      b.sort_by_priority
       num_choices = b.choices.size
       ballot_handicap = num_choices.to_f / (1..num_choices).sum
       b.choices.each do |c|
@@ -10,11 +12,42 @@ module PollsHelper
           results[c.slug] ||= {}
           results[c.slug][:original] ||= c.original       
           results[c.slug][:tally] ||= 0.0 
-          results[c.slug][:tally] += (((num_choices - c.priority) / num_choices.to_f) * ballot_handicap)
+          results[c.slug][:tally] += (((num_choices - c.priority) / num_choices.to_f) * ballot_handicap) / poll.ballots.cast.count
         end
       end
     end
-    results = results.to_a.collect {|r| r[1]}.sort {|x,y| y[:tally] <=> x[:tally] }
+
+    if related
+      extra_results = {}
+      results.each do |r|
+        original = r[1][:original]
+        tally = r[1][:tally]
+        response = Wordnik.word.get_related_words(original, :useCanonical => 'true')
+        if response && !response.empty?
+          related = response[0]["words"]
+        else
+          related = []
+        end
+        related.each do |original|
+          slug = original.downcase.gsub(/[^a-z0-9]+/, '')
+          extra_results[slug] ||= {}
+          extra_results[slug][:original] ||= original       
+          extra_results[slug][:tally] ||= 0.0 
+          extra_results[slug][:tally] += ((tally / 2.0)*(1.0 / related.size))
+        end
+        if !related.empty?
+           tally = tally / 2.0
+        end
+        extra_results[r[0]] ||= {}
+        extra_results[r[0]][:original] ||= original
+        extra_results[r[0]][:tally] ||= 0.0
+        extra_results[r[0]][:tally] += tally
+      end
+      results = extra_results.to_a.collect {|r| r[1]}.sort {|x,y| y[:tally] <=> x[:tally] }
+    else
+      results = results.to_a.collect {|r| r[1]}.sort {|x,y| y[:tally] <=> x[:tally] }
+    end
+    results
   end
 
 end
--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -22,6 +22,12 @@ class Ballot
     super(attributes)
   end
 
+  def sort_by_priority
+    reordered_choices = self.choices.sort {|x,y| x.priority <=> y.priority }
+    self.choices.clear
+    reordered_choices.each { |choice| self.choices.create(choice.attributes) } 
+  end
+  
   protected
 
   def destroy_blank_choices
@@ -31,12 +37,6 @@ class Ballot
   
   def ensure_one_choice
      self.choices.create if self.choices.size == 0
-  end
-
-  def sort_by_priority
-    reordered_choices = self.choices.sort {|x,y| x.priority <=> y.priority }
-    self.choices.clear
-    reordered_choices.each { |choice| self.choices.create(choice.attributes) } 
   end
 
   def mark_as_cast
