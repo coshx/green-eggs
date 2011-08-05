7a2aeebf167fbd8d18b47069824159bf8638f903
Gabe Kopley <gabe@coshx.com>
2011-08-05 00:10:33 -0600

    create a new choice when you hit enter on the last choice

--- app/assets/javascripts/ballots.js
+++ app/assets/javascripts/ballots.js
@@ -1,15 +1,23 @@
 
 jQuery(document).ready(function(){
 
-	jQuery("a#add_choice").bind("click",function(){
+  var addChoice = function() {
+    var num = jQuery('form ol li').size();
+   jQuery('<li id="ballot_choices_attributes_'+num+'_original_input" class="string optional"><label for="ballot_choices_attributes_'+num+'_original">Original</label><input type="text" name="ballot[choices_attributes]['+num+'][original]" id="ballot_choices_attributes_'+num+'_original"></li>').appendTo("form.formtastic.ballot div.fields > ol");
+  };
 
-   	var num = jQuery('form ol li').size();
-    	jQuery('<li id="ballot_choices_attributes_'+num+'original_input" class="string optional"><label for="ballot_choices_attributes_'+num+'_original">Original</label><input type="text" name="ballot[choices_attributes]['+num+'][original]" id="ballot_choices_attributes_'+num+'_original"></li>').appendTo("form.formtastic.ballot div.fields > ol");
-	
 	jQuery("#sortable").sortable();	   
- 	
+	jQuery("a#add_choice").bind("click",function(){
+    addChoice();	
 	});
 
+  jQuery("div.fields ol input[type='text']").live('keypress', function(e) {
+    if(this === jQuery("div.fields ol input[type='text']").last()[0] && e.keyCode == 13) { 
+      addChoice();
+      jQuery("div.fields ol input[type='text']").last().focus();
+      return false;
+    }
+  });
 
 	jQuery("input[value='Cast your vote']").bind("click",function(){
      // append the priority hidden fields before submitting
