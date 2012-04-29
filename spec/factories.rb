9f5897e0ce37cc5866e64f75a909d73d7024408d
Gabe Kopley <gabe@coshx.com>
2012-04-29 14:45:16 -0700

    update factorygirl api aftermath

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,39 +1,39 @@
 FactoryGirl.define do
 
-factory :poll do |p|
-  p.name "What to order for Friday lunch?"
-  p.owner_email "stewie@example.com"
-end
-
-factory :poll_with_ballot, :parent => :poll do |p|
-  p.after_create do |poll|
-    ballot = FactoryGirl.build(:ballot)
-    poll.ballots << ballot
+  factory :poll do
+    name "What to order for Friday lunch?"
+    owner_email "stewie@example.com"
   end
-end
 
-factory :ballot do |b|
-  b.email "sonia@example.com"
-end
+  factory :poll_with_ballot, :parent => :poll do
+    after_create do |poll|
+      ballot = FactoryGirl.build(:ballot)
+      poll.ballots << ballot
+    end
+  end
 
-factory :choice do |c|
-  c.sequence(:original) {|i| "unique_choice_#{i}"}
-end
+  factory :ballot do
+    email "sonia@example.com"
+  end
 
-factory :first_choice, :parent => :choice do |c|
-  c.priority 0
-end
+  factory :choice do
+    sequence(:original) {|i| "unique_choice_#{i}"}
+  end
 
-factory :second_choice, :parent => :choice do |c|
-  c.priority 1
-end
+  factory :first_choice, :parent => :choice do
+    priority 0
+  end
 
-factory :third_choice, :parent => :choice do |c|
-  c.priority 2
-end
+  factory :second_choice, :parent => :choice do
+    priority 1
+  end
 
-factory :blank_choice, :parent => :choice do |c|
-  c.original ""
-end
+  factory :third_choice, :parent => :choice do
+    priority 2
+  end
+
+  factory :blank_choice, :parent => :choice do
+    original ""
+  end
 
 end
