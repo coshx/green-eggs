173467002403eb91f37430a03729a6e32d4a18b8
Ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2012-05-24 13:28:18 -0400

    adds the description in the email (accidentally ommitted in the previous commit)

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -3,6 +3,7 @@ class InviteMailer < ActionMailer::Base
 
   def invite_to_vote(ballot)
     @ballot = ballot
+    @poll = ballot.poll
     mail(:to => ballot.email,
          :subject => "Invitation to vote on #{ballot.poll.name}")
   end
