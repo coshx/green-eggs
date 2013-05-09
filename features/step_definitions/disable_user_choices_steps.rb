e42aede969d48d97878f8bd0f4f2228cdbad657c
Michael Makarov <info1117@gmail.com>
2013-05-09 16:54:11 -0700

    Creates a first broken test and pass it

--- features/step_definitions/disable_user_choices_steps.rb
+++
@@ -1,7 +0,0 @@
-Then /^I should not see a text input$/ do
-  page.should_not have_css("input[type='text']")
-end
-
-Then /^I should see a text input$/ do
-  page.should have_css("input[type='text']")
-end
---
+++ spec/acceptance/create_poll_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+feature %{
+  I want to play with polls
+} do
+
+  background do
+  end
+
+  scenario "Create a new poll", :js => true do
+    visit "/create"
+    fill_in "question", :with => 'The best way to seduce a rabbit?'
+    fill_in "email", :with => "rabbit@stewieland.com"
+    click_button "go_to_preseed"
+    page.should have_content('The best way to seduce a rabbit?')
+    page.should have_content('Pre-seed answers')
+    fill_in "answer", :with => "show a huge carrot"
+    click_button "add-choice"
+    fill_in "answer", :with => "show a huge rabbit of opposite sex"
+    click_button "add-choice"
+    fill_in "answer", :with => "show a gun"
+    click_button "add-choice"
+    click_button "save-poll"
+
+  end
+
+
+end
